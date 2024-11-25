ef65b7069aa63d55f7ceadac21b0b547b1591404
mehmet12ws <126833226+mehmet12ws@users.noreply.github.com>
2024-11-25 22:42:42 +0300

    Update raid.js

--- raid.js
+++ raid.js
@@ -20,9 +20,11 @@ const timeWindow = 10 * 1000;
 
 const imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg_jQ5pPIz5pSy-WT9Tcu9KIKUK9jRJDNudA&s";
 
-client.on("ready" , () => {
-client.user.setActivity('Test')
-}}
+client.on("ready", () => {
+    client.user.setActivity('Test');
+});
+
+
     
 
 
